386c7d9a9cb7aa8629b74d8e3c0a593f3c75e51a
kipruto <barxwells@gmail.com>
2019-02-06 02:54:05 +0300

    [Finishes #163726503] set up blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from instance.config import app_config
+from app.api.v1.views import api as v1
 
 def create_app(config_name):
     """
@@ -10,5 +11,6 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    app.register_blueprint(v1, url_prefix='/api/v1/')
 
     return app

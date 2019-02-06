8e69e1f75a5d9dd1efd9d7254cf6fdebe3c45cda
kipruto <barxwells@gmail.com>
2019-02-06 03:58:35 +0300

    [Finishes #163747398] Added create party feature

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,3 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, request, jsonify
+from .models import Party
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
+class PartiesEndPoint:
+    """Party API Endpoints"""
+
+    @api.route('/parties', methods=["POST"])
+    def post():
+        """ Create party endpoint """
+        data = request.get_json()
+        name = data['name']
+        headquarters = data['hqAddress']
+        logoUrl = data['logoUrl']
+
+        party = Party().create_party(name, headquarters, logoUrl)
+        return make_response(jsonify({
+            "status": 201,
+            "data": party
+        }), 201)

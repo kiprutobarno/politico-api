9f9173e7dcdff64ee07ddb26e88918fd9224acdb
kipruto <barxwells@gmail.com>
2019-02-07 12:01:29 +0300

    [Finishes #163688617] Added create-political-office feature

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, make_response, request, jsonify
-from .models import Party
+from .models import Party, Office
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 
@@ -8,7 +8,7 @@ class PartiesEndPoint:
     """Party API Endpoints"""
 
     @api.route('/parties', methods=["POST"])
-    def post():
+    def post_party():
         """ Create party endpoint """
         data = request.get_json()
         name = data['name']
@@ -23,7 +23,7 @@ class PartiesEndPoint:
         }), 201)
 
     @api.route('/parties', methods=["GET"])
-    def get():
+    def get_parties():
         """ Get all parties endpoint """
         parties = Party().get_all_parties()
         return make_response(jsonify({
@@ -43,7 +43,7 @@ class PartiesEndPoint:
         }), 200)
 
     @api.route('/parties/<int:id>', methods=['PATCH'])
-    def patch(id):
+    def patch_party(id):
         """ Update specific political party """
         data = request.get_json()
 
@@ -55,10 +55,27 @@ class PartiesEndPoint:
         }), 200)
 
     @api.route('/parties/<int:id>', methods=["DELETE"])
-    def delete(id):
+    def delete_party(id):
         """ Delete specific political party """
         party = Party().delete_party(id)
         return make_response(jsonify({
             "status": 200,
             "message": "Success"
         }), 200)
+
+
+class OfficesEndpoint:
+    """Office API Endpoints"""
+
+    @api.route('/offices', methods=['POST'])
+    def post_office():
+        """ Create office endpoint """
+        
+        data = request.get_json()
+        
+        office = Office().create_office(data['type'], data['name'])
+        return make_response(jsonify({
+            "status": 201,
+            "message": "Success",
+            "data": office
+        }), 201)

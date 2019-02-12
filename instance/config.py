11d7ee6a0c0af9b602d53bac9b0535620657fd34
kipruto <barxwells@gmail.com>
2019-02-13 02:45:21 +0300

    [Finishes #163919050] API version 2 project directories and database setup

--- instance/config.py
+++ instance/config.py
@@ -10,12 +10,16 @@ class DevelopmentConfig(Config):
     """Configurations for Development"""
     DEBUG = True
     TESTING = False
+    DATABASE_URI = os.getenv('DATABASE_URI')
+    os.environ['ENV'] = 'development'
 
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database"""
     TESTING = True
     DEBUG = True
+    DATABASE_URI = os.getenv('DATABASE_URI')+'_test'
+    os.environ['ENV'] = 'testing'
 
 
 app_config = {

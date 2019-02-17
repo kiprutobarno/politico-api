391f8984dfd504a5c78bcf26737de5b205328b57
kipruto <barxwells@gmail.com>
2019-02-17 21:39:02 +0300

    [Finishes #163910459] Added register-candidate feature

--- tests/v2/test_candidate.py
+++ tests/v2/test_candidate.py
@@ -9,11 +9,9 @@ class CandidateTestCase(BaseTestCase):
     def test_register_candidate(self):
         """ Test that endpoint can create office"""
         super().create_user(user)
-        super().create_user(admin_user)
         login=super().login_user(admin_user_login)
         login_content = json.loads(login.data.decode('utf-8'))
         token=[d['token'] for d in login_content['data']][0]
-        response = super().create_office(office, token)
         response=super().register_candidate(candidate, token)
-        # response_content =  json.loads(response.data.decode())
-        self.assertEqual(response.status_code, 201)
+        response_content =  json.loads(response.data.decode('utf-8'))
+        self.assertTrue(response_content['status'] == 201)
